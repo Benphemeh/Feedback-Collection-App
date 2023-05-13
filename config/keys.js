b23ce5a9823bce9fbeaa0177b5c75b06a7bc6bb5
Adewole oluwanifemi <106755296+Benphemeh@users.noreply.github.com>
2023-05-13 08:33:18 +0100

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: '61657319151-d2tfcrd7i0i9vp3hm90aqpcqs33m11ur.apps.googleusercontent.com',
+  googleClientSecret: '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',
+  mongoURI: 'mongodb+srv://beniphemeh11:DmWd5NmktLK3sAXO@cluster0.xwze6ax.mongodb.net/test',
+  cookieKey: 'jlkjfdklajfdajiejijijfdf',
+};
+
+
+
